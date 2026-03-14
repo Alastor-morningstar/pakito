1fa809851fe5b8f59d1cc13b4bd36dc54cb19e94
irvin_zamora <id.zamora23@info.uas.edu.mx>
2026-03-14 01:04:15 -0700

    Comit de prueb

---
+++ app/src/main/java/com/fic/botonclick/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.fic.botonclick
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private var contador = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val boton = findViewById<Button>(R.id.Boton)
+
+        boton.setOnClickListener {
+            contador++
+            boton.text = contador.toString()
+        }
+        Log.d("Ciclo", "onCreate")
+    }
+}
